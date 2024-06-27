61e26c82ff38792b959bc23275624bb8bdae4d4a
narendra2313 <narendra.gupta@sportzinteractive.net>
2024-06-27 14:22:09 +0530

    return type update.

--- lb_content_listing/src/main/java/com/example/lb_content_listing/data/repository/LBRepositoryImpl.kt
+++ lb_content_listing/src/main/java/com/example/lb_content_listing/data/repository/LBRepositoryImpl.kt
@@ -7,6 +7,7 @@ import com.example.base.helper.Resource
 import com.example.base.helper.safeApiCall
 import com.example.lb_content_listing.business.repository.LBRepository
 import com.example.lb_content_listing.data.model.layoutbuilder.Content
+import com.example.lb_content_listing.data.model.layoutbuilder.Module
 import com.example.lb_content_listing.data.service.LBService
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
@@ -17,18 +18,20 @@ class LBRepositoryImpl @Inject constructor(
     private val lbService: LBService,
     @IoDispatcher private val ioDispatcher: CoroutineDispatcher,
 ) : LBRepository {
-    override fun getLBListing(url: String): Flow<Resource<Content?>> {
+    override fun getLBListing(url: String): Flow<Resource<List<Module>?>> {
         return flow {
 
             val response = safeApiCall(ioDispatcher) {
                 lbService.getLBPageListing(url = url)
             }
 
-            val resource = object : ApiResultHandler<BaseResponse<Content>, Content?>(
+            val resource = object : ApiResultHandler<BaseResponse<Content>, List<Module>>(
                 response
             ) {
-                override suspend fun handleSuccess(resultObj: BaseResponse<Content>): Resource<Content?> {
-                    return Resource.Success(data = resultObj.content)
+                override suspend fun handleSuccess(resultObj: BaseResponse<Content>): Resource<List<Module>?> {
+                    val module = resultObj.content?.module?.filter { it.showInApp == 1 }
+                        ?.sortedBy { it.metaInfo?.widgetOrder ?: 0 }.orEmpty()
+                    return Resource.Success(data = module)
                 }
             }.getResult()
 
